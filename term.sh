8edf8ace60d3aa7f47061acb612df5ae3bc018c9
akashdeepb <36377879+akashdeepb@users.noreply.github.com>
2018-09-26 07:25:51 +0530

    Added Files

---
+++ term.sh
@@ -0,0 +1,16 @@
+CommandLines=$(cat bannerCommands | wc -l)
+BannerNumber=$(((RANDOM%$CommandLines)+1))
+command=$(sed $BannerNumber'!d' bannerCommands)
+if [ $BannerNumber -lt 6 ];then
+	textFile=bigText
+elif [ $BannerNumber -gt 5 ];then
+	textFile=smallText
+fi
+colorLines=$(cat textCol | wc -l)
+colorNumber=$(((RANDOM%$colorLines)+1))
+color=$(sed $colorNumber'!d' textCol)
+WelcomeLines=$(cat $textFile | wc -l)
+WelcomeNumber=$(((RANDOM%$WelcomeLines)+1))
+text=$(sed $WelcomeNumber'!d' $textFile)
+echo -e $color
+$command $text
---
+++ termwel.sh
@@ -0,0 +1,11 @@
+echo -e "\t\t W E L C O M E     T O     T E R M W E L "
+echo " An Open Source Project to Welcome users to Terminal "
+echo -e "\033[1;36m github : https://github.com/akashdeepb/termwel \033[1;m "
+wd=$(pwd)
+echo -e "\n # This is TERMINAL WELCOME ### \n" >>~/.bashrc
+echo -n "cd " >>~/.bashrc && echo $wd >> ~/.bashrc && echo "./term.sh" >>~/.bashrc && echo "cd" >>~/.bashrc
+echo -e "\n # END OF TERMINAL WELCOME ### \n" >>~/.bashrc
+echo -e "\n\n\t --  INSTALLING REQUIREMENTS -- "
+sudo apt-get install figlet
+sudo apt-get install toilet
+sudo apt-get install sysvbanner
