d883218e9846fb31da7aeca729224ccff741321f
akashdeepb <36377879+akashdeepb@users.noreply.github.com>
2018-09-26 07:29:16 +0530

    Update termwel.sh

--- termwel.sh
+++ termwel.sh
@@ -1,6 +1,7 @@
 echo -e "\t\t W E L C O M E     T O     T E R M W E L "
 echo " An Open Source Project to Welcome users to Terminal "
 echo -e "\033[1;36m github : https://github.com/akashdeepb/termwel \033[1;m "
+chmod +x term.sh
 wd=$(pwd)
 echo -e "\n # This is TERMINAL WELCOME ### \n" >>~/.bashrc
 echo -n "cd " >>~/.bashrc && echo $wd >> ~/.bashrc && echo "./term.sh" >>~/.bashrc && echo "cd" >>~/.bashrc
