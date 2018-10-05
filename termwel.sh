e14162e64d50a4b9e1ea666658700cb64351d10c
akashdeepb <36377879+akashdeepb@users.noreply.github.com>
2018-10-05 12:14:07 +0530

    Updated termwel.sh

--- termwel.sh
+++ termwel.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 echo -e "\t\t W E L C O M E     T O     T E R M W E L "
 echo " An Open Source Project to Welcome users to Terminal "
 echo -e "\033[1;36m github : https://github.com/akashdeepb/termwel \033[1;m "
